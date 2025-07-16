5c83c5a2b14bb51504678fc06a3abdceaeb5be5c
Vijay Arjilli <150048469+ArjilliVijay@users.noreply.github.com>
2025-07-16 23:27:35 +0530

    Delete login.js

--- login.js
+++
@@ -1,9 +0,0 @@
-// js/login.js
-document.getElementById('loginForm').addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  const fullName = e.target.name.value.trim();  // Get the actual full name
-
-  alert(`${fullName}, login successful!`);
-  window.location.href = 'dashboard.html';
-});
